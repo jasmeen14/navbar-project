7277d9e3cbbd0615b5ea50ccb2d60f0db191897b
jasmeen14 <jasmeen6199@gmail.com>
2022-02-19 20:33:41 +0100

    updated files

---
+++ main.js
@@ -0,0 +1,14 @@
+const navToggle= document.querySelector(".nav-toggle");
+const links= document.querySelector(".links");
+navToggle.addEventListener("click",function(){
+ //console.log(links.classList.contains("random"));   
+ //if(links.classList.contains("show-links"))
+// {
+    // links.classList.remove("show-links");
+// }
+ //else{
+     //links.classList.add("show-links");
+ //}
+ //can be done in just 1 line below
+ links.classList.toggle("show-links");
+});
